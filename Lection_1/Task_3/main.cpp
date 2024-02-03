9317906099ccf48b0f142d5ad2a46be1b9f67c83
Sergiy <nomi45148@gmail.com>
2024-02-03 17:30:11 +0100

    Task_3 completed

---
+++ Lection_1/Task_3/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int height;
+    cout << "Enter the height: ";
+    cin >> height;
+
+    for (int current_row = 0; current_row < height; ++current_row) {
+        for (int spaces = 0; spaces < height - current_row - 1; ++spaces)
+            cout << " ";
+
+        for (int stars = 0; stars < 2 * current_row + 1; ++stars)
+            cout << "*";
+
+        cout << std::endl;
+    }
+
+    for (int trunk_spaces = 0; trunk_spaces < height - 1; ++trunk_spaces)
+        cout << " ";
+    cout << "*" << std::endl;
+
+    return 0;
+}
+
