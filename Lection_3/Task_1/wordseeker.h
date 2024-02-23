39493afdce9f87b720cc8b3da37938fd8cfa61b7
Sergiy <nomi45148@gmail.com>
2024-02-23 16:18:48 +0100

    new slots for the funcs

--- Lection_3/Task_1/wordseeker.h
+++ Lection_3/Task_1/wordseeker.h
@@ -2,17 +2,15 @@
 #define WORDSEEKER_H
 
 #include <QMainWindow>
-#include <QLineEdit>
-#include <QTextEdit>
-#include <QRadioButton>
-#include <QSpinBox>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class WordSeeker; }
+namespace Ui {
+class WordSeeker;
+}
 QT_END_NAMESPACE
 
-class WordSeeker : public QMainWindow
-{
+class WordSeeker : public QMainWindow {
     Q_OBJECT
 
 public:
@@ -21,13 +19,14 @@ public:
 
 private slots:
     void counter();
-    void onEncryptionSelected();
-    void onDecryptionSelected();
+    void processText();
 
 private:
     Ui::WordSeeker *ui;
-    QSpinBox *spinBox; // Добавляем указатель на QSpinBox
-    void encryptDecrypt(QString& text, int shift);
+
+    // Новые методы для шифрования и дешифрования
+    QString encryptVigenere(const QString &text, const QString &key);
+    QString decryptVigenere(const QString &text, const QString &key);
 };
 
-#endif // WORDSEEKER_H
+#endif
